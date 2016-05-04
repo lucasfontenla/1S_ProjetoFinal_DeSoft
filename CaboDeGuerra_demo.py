f4e8720005a2998959860015ddbca8d711442655
guigs10mil <gui.m.s@hotmail.com>
2016-05-04 15:24:08 -0300

    Added files via upload
    jogo offline sem a mensagem "Ganhou"

--- CaboDeGuerra_demo.py
+++ CaboDeGuerra_demo.py
@@ -52,6 +52,15 @@ start0 = pygame.image.load("CDG_Images/START_0.png")
 start0 = pygame.transform.scale(start0, (200, 200))
 start1 = pygame.image.load("CDG_Images/START_1.png")
 start1 = pygame.transform.scale(start1, (200, 200))
+n3 = pygame.image.load("CDG_Images/No_3.png")
+n3 = pygame.transform.scale(n3, (300, 300))
+n2 = pygame.image.load("CDG_Images/No_2.png")
+n2 = pygame.transform.scale(n2, (300, 300))
+n1 = pygame.image.load("CDG_Images/No_1.png")
+n1 = pygame.transform.scale(n1, (300, 300))
+win_bg = pygame.image.load("CDG_Images/White_Square50.png")
+win_bg = pygame.transform.scale(win_bg, (800, 600))
+#win_bg = pygame.Color.a(30)
 
 def p_marrom1(x, y):
     character("CDG_Images/char1.1.png", (x, y), right)
@@ -90,7 +99,12 @@ def mountain_bg(x, y):
     gameDisplay.blit((mountain), (x, y))
     
 def cloud1(x, y):
-    gameDisplay.blit(pygame.transform.scale((pygame.image.load("CDG_Images/Pixel_cloud1.png")), (400, 400)), (x, y))
+    if x > 805:
+        clxx = x - 400
+    else:
+        clxx = x
+    gameDisplay.blit(pygame.transform.scale((pygame.image.load("CDG_Images/Pixel_cloud1.png")), (400, 400)), (clxx, y))
+    
     
 def PM_bg(x, y):
     gameDisplay.blit((PM), (x, y))
@@ -100,9 +114,17 @@ def QZ_bg(x, y):
 def TugWar(x, y):
     gameDisplay.blit((Tug), (x, y))
     
+def N3(x, y):
+    gameDisplay.blit((n3), (x, y))
+def N2(x, y):
+    gameDisplay.blit((n2), (x, y))
+def N1(x, y):
+    gameDisplay.blit((n1), (x, y))
     
-#def puxar (player, x, y):
+def win(x, y):
+    gameDisplay.blit((win_bg), (x, y))
     
+
     
 px = 200
     
@@ -121,7 +143,11 @@ def camera(x):
     cam = Tela*(1/(1 + math.e**-(0.7*x)))
     return -cam
     
-x = -20
+def camera_win(x):
+    cam = 600*(1/(1 + math.e**-(2*x)))
+    return -cam
+    
+x = -12
 
 select = False
 
@@ -134,12 +160,27 @@ while not select:
         if event.type == pygame.QUIT:
             pygame.quit()
 #           sys.exit()
+    cloud1(cl_x1 + 60, -100 + camera(x) + Tela - 2000)
+    cloud1(cl_x1 + 600, 0 + camera(x) + Tela - 2000)
+    cloud1(cl_x1 - 50, -150 + camera(x) + Tela - 1600)
+    cloud1(cl_x1 + 500, 50 + camera(x) + Tela - 1600)
+    
+    cl_x1 += 0.3    
+    cl_x2 += 0.3
+    
+    if cl_x1 > 805:
+        cl_x1 = -400
+    
+    if cl_x2 > 805:
+        cl_x2 = -400
+        
     
     Tug = pygame.image.load("CDG_Images/Tug_War.png")
     Tug = pygame.transform.scale(Tug, (800 + TitleSize, 800 + TitleSize))
             
     TitleSize = int((15*(1+math.sin(2*3.14*n*0.01)))*0.5)
     n -= 1
+    
             
             
     TugWar(40 - TitleSize/3, -200 - TitleSize/3) 
@@ -154,8 +195,9 @@ while not select:
 
 
 ######################################################################################################
+contadorFrames = 0
 
-
+w = -7
 
 #condição p/ parar o jogo
 crashed = False
@@ -167,13 +209,13 @@ while not crashed:
         if ev.type == pygame.QUIT:
             crashed = True
         if ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_q and px < 400:
+            if ev.key == pygame.K_q and px < 400 and px > 0:
                 px += 5  
-            if ev.key == pygame.K_p and px > 0:
+            if ev.key == pygame.K_p and px > 0 and px < 400:
                 px -= 5
-            if ev.key == pygame.K_z and px < 400:
+            if ev.key == pygame.K_z and px < 400 and px > 0:
                 px += 5
-            if ev.key == pygame.K_m and px > 0:
+            if ev.key == pygame.K_m and px > 0 and px < 400:
                 px -= 5
     
 
@@ -196,7 +238,7 @@ while not crashed:
     cloud1(cl_x1 + 400, 100 + camera(x) + Tela - 400)
     cloud1(cl_x1, -50 + camera(x) + Tela)
     cloud1(cl_x2, 0 + camera(x) + Tela)
-    cl_x1 += 0.3    
+    cl_x1 += 0.3
     cl_x2 += 0.3
     
     if cl_x1 > 805:
@@ -204,38 +246,61 @@ while not crashed:
     
     if cl_x2 > 805:
         cl_x2 = -400
-        
     
-        
-    QZ_bg(90,215 + camera(x) + Tela)
-    PM_bg(570,215 + camera(x) + Tela)
+    taxa = 1.5    
     
-    x += 0.1    
+    QZ_bg(90,215 + camera(x) *taxa + 1000 + Tela)
+    PM_bg(570,215 + camera(x) *taxa + 1000 + Tela)
+    
+    x += 0.1
+
     
     if pygame.key.get_pressed()[pygame.K_q] and px < 400 or pygame.key.get_pressed()[pygame.K_z] and px < 400:
-        p_marrom2(185,206 + 25 + camera(x) + Tela)
+        p_marrom2(185,206 + 25 + camera(x) *taxa + 1000 + Tela)
     else:
-        p_marrom1(185,206 + 25 + camera(x) + Tela)
+        p_marrom1(185,206 + 25 + camera(x) *taxa + 1000 + Tela)
     if pygame.key.get_pressed()[pygame.K_p] and px < 400 or pygame.key.get_pressed()[pygame.K_m] and px < 400:
-        p_rosa20(567, 206 + 25 + camera(x) + Tela)
+        p_rosa20(567, 206 + 25 + camera(x) *taxa + 1000 + Tela)
     else:
-        p_rosa10(567, 206 + 25 + camera(x) + Tela)
+        p_rosa10(567, 206 + 25 + camera(x) *taxa + 1000 + Tela)
     
     if pygame.key.get_pressed()[pygame.K_q] and px < 400 or pygame.key.get_pressed()[pygame.K_z] and px < 400:
-        corda_i(198, 245 + 25 + camera(x) + Tela)
+        corda_i(198, 245 + 25 + camera(x) *taxa + 1000 + Tela)
     elif pygame.key.get_pressed()[pygame.K_p] and px < 400 or pygame.key.get_pressed()[pygame.K_m] and px < 400:
-        corda_i(202, 245 + 25 + camera(x) + Tela)
+        corda_i(202, 245 + 25 + camera(x) *taxa + 1000 + Tela)
     else:
-        corda_i(200, 245 + 25 + camera(x) + Tela)
+        corda_i(200, 245 + 25 + camera(x) *taxa + 1000 + Tela)
+
     
-    
-    
-    pygame.draw.rect(gameDisplay, (0,0,255), (200,300 + camera(x) + Tela,400,50), 0)
-    pygame.draw.rect(gameDisplay, (255,0,0), (200,300 + camera(x) + Tela,px,50), 0)
+    pygame.draw.rect(gameDisplay, (0,0,255), (200,300 + camera(x) *taxa + 1000 + Tela,400,50), 0)
+    pygame.draw.rect(gameDisplay, (255,0,0), (200,300 + camera(x) *taxa +1000 + Tela,px,50), 0)
     button_pressed = False
     
     
-
+    
+    if px < 1:
+        s = pygame.Surface((800,600))  # the size of your rect
+        s.set_alpha(200)                # alpha level
+        s.fill((255,255,255))           # this fills the entire surface
+        gameDisplay.blit(s, (0,600 + camera_win(w)))
+        w += 0.1
+        
+    if px > 399:
+        s = pygame.Surface((800,600))  # the size of your rect
+        s.set_alpha(200)                # alpha level
+        s.fill((255,255,255))           # this fills the entire surface
+        gameDisplay.blit(s, (0,600 + camera_win(w)))
+        w += 0.1
+        
+    if contadorFrames > 30 and contadorFrames < 90:
+        N3(400 - 200*0.5, 300 - 300*0.5)
+    if contadorFrames > 90 and contadorFrames < 150:
+        N2(400 - 200*0.5, 300 - 300*0.5)
+    if contadorFrames > 150 and contadorFrames < 210:
+        N1(400 - 200*0.5, 300 - 300*0.5)
+    
+    contadorFrames += 1
+    
     pygame.display.update()
     
     #how much FPS
@@ -244,3 +309,7 @@ while not crashed:
 #quitar o jogo quando sair do loop    
 pygame.quit()
 quit()
+
+
+
+
