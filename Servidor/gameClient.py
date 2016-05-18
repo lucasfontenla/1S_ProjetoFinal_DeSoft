a236a8eb63b2719a27d0e47136b7118af5025e01
lucasfontenla <lucascfontenla@hotmail.com>
2016-05-18 08:51:09 -0300

    Servidor e cliente atualizados

--- Servidor/gameClient.py
+++ Servidor/gameClient.py
@@ -1,25 +1,32 @@
 import time, socket
-  
+from threading import Thread  
+
 #O programa segue uma ordem "cronológica", na qual a função anterior chama a próxima. Nas divisões, são momentos em que a anterior não chamou a seguinte 
 
 class Client():
 	def __init__(self):
-		self.port = 5000
+		self.port = 80
 
-	def serverHost(self, serverIP):
+	def showMyHost(self):
+		return [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][1]
+
+	def setHost(self, serverIP):
 		self.hostServer = serverIP
+
+	def setName(self, name):
+		self.Name = name
 
 	def clientStart(self):
 		self.connection_status = 'none'
-		self.c = socket.socket()
-		self.c.connect((self.hostServer, self.port))
+		self.clientTCP = socket.socket()
+		self.clientTCP.connect((self.hostServer, self.port))
 
-		self.c.setblocking(0)
+		self.clientTCP.setblocking(0)
 		timeout0 = time.clock()
 
 		data = str()
 
-		self.checkStatus('trying to connect')
+		self.checkStatus('Trying to connect...')
 
 		while True:
 				if time.clock()-timeout0 >= 20:
@@ -28,7 +35,7 @@ class Client():
 				else:
 					timeout = False
 					try:
-						data = self.c.recv(1024)
+						data = self.clientTCP.recv(1024)
 						if data:
 							if data == 'connected as principal'.encode():
 								self.checkStatus('Connected as principal')
@@ -41,68 +48,173 @@ class Client():
 
 		if timeout:
 			self.checkStatus('Error 001 Timeout: could not connect to server in time')
+			return 'timeout'
 		else:
 			if self.connection_status == 'Connected as principal':
 				return 'done: principal'
 			else:
 				return 'done: not principal'
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
-	def closeConnection(self):
-		self.c.send('close'.encode())
-#----------------------------------------------------------------------------------------------------------------------------------------------------------------		
-	def getMyTeam(self):	
-		self.myteam = 'none'
-		self.c.setblocking(1)
-		data = self.c.recv(1024)
-		self.myteam = str(data)
+	def NameSend(self):
+		print('Waiting to send Name')
+		while True:
+			try:
+				wait_name = self.clientTCP.recv(1024)
+				if wait_name == 'send your name'.encode():
+					self.clientTCP.send(str(self.Name).encode())
+					print('Name sent')
+					break
+			except: pass
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
 	def checkStatus(self, value):
 		self.connection_status = value
 		print(value)
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
+	def closeConnection(self):
+		self.clientTCP.send('close'.encode())
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------		
+	def sendMyTeam(self, team): #blue, red or random
+		print('Waiting to send my team...')	
+		while True:
+			try:
+				wait_team = self.clientTCP.recv(1024)
+				if wait_team == 'send your team'.encode():
+					self.clientTCP.send(str(team).encode())
+					break
+			except: pass
+
+		print("Team sent")
+		print('Waiting team confirmation...')
+		while True:
+			try:
+				team = self.clientTCP.recv(1024)
+				team2 = str(team).split("'")
+				team3 = str(team2[0]).split('"')
+				self.myteam = str(team3[1])
+				self.teamsDisplay = (team2[1], team2[2])
+				break
+			except: pass
+		print('Got my team')
+		return 'got my team'
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------
 	def myTeam(self):
 		return self.myteam
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
+	def displayTeam(self):
+		return self.teamsDisplay
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------
 	def StartGame(self):
-		self.c.send('start game'.encode())
+		self.clientTCP.send('start game'.encode())
 		self.waitStart()
 
 	def waitStart(self):
 		while True:
-			data = self.c.recv(1024)
-			if data == 'start'.encode():
-				break
-			else:
-				pass
+			try:
+				data = self.clientTCP.recv(1024)
+				if data == 'start'.encode():
+					break
+			except: pass
 		print('started')
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------
 	def sendPackages(self, value):
-		print(value)
-		self.c.send(str(value).encode())
+		self.totalclicks(int(value))
+		self.clientTCP.send((self.Name + "'" + str(value)).encode())
+
+	def totalclicks(self, value):
+		self.myclicks = value
+
+	def receivePoints(self):
+		while True:
+			try:
+				percentage = self.clientTCP.recv(1024)
+				print(percentage)
+				if percentage == 'game ended'.encode() or percentage == 'restart game'.encode():
+					self.getHighScore()
+					break
+				break
+			except: pass
+		return percentage
+
+	def getHighScore(self):
+		print('Waiting Highscore...')
+		while True:
+			try:
+				self.highscore = self.clientTCP.recv(1024)
+				break
+			except: pass
+
+	def myHighscore(self):
+		return self.highscore
 
 	def endServer(self):
-		self.c.send('end server'.encode())
-		self.c.close()		
+		self.clientTCP.send('close'.encode())
+		self.clientTCP.close()
 
-client = Client()
-client.serverHost('127.0.0.1')
-value = client.clientStart()
-if value == 'done: principal':
-	time.sleep(10)
-	client.closeConnection()
-	print(client.connection_status)
-	client.getMyTeam()
-	print(client.myTeam())
-	client.StartGame()
-	for i in range(0, 100):
-		client.sendPackages(500)
-		time.sleep(.5)
-	client.endServer()
-else:
-	print(client.connection_status)
-	client.getMyTeam()
-	print(client.myTeam())
-	client.waitStart()
-	for i in range(0, 100):
-		client.sendPackages(500)
-		time.sleep(.5)
+# client = Client()
+# client.setHost('127.0.0.1')
+# client.setName('Guizaum')
+# contador = 0
+# breakwhile = False
+
+# while breakwhile == False:
+# 	value = client.clientStart()
+# 	receiveKill = False
+
+# 	def SEND1():
+# 		for i in range(0, 10):
+# 				client.sendPackages(1)
+# 				time.sleep(.5)
+# 		global contador		
+# 		contador += 1
+# 		if contador == 3:
+# 			client.endServer()
+# 		else:
+# 			client.restartGame()
+				
+# 		receiveKill = True	
+
+# 	def SEND2():
+# 		for i in range(0, 10):
+# 				client.sendPackages(1)
+# 				time.sleep(.5)
+
+# 	def RECEIVE():
+# 		receiveDone = False
+# 		while True:
+# 			value = client.receivePoints()
+# 			print(str(value))
+# 			if value == 'game ended'.encode() or value == 'restart game'.encode():# or receiveKill:
+# 				break
+
+# 		print(client.myHighscore())
+
+# 		if value == 'game ended'.encode():
+# 			global breakwhile
+# 			beakwhile = True	
+
+# 	if value == 'done: principal':
+# 		time.sleep(10)
+# 		client.closeConnection()
+# 		client.NameSend()
+# 		value = client.sendMyTeam('blue')
+# 		if value == 'got my team':
+# 			print(client.myTeam())
+# 			print(client.displayTeam())
+# 			client.StartGame()
+# 			t1 = Thread(target=SEND1)
+# 			t2 = Thread(target=RECEIVE)
+# 			t1.start()
+# 			t2.start()
+# 	else:
+# 		client.NameSend()
+# 		value = client.sendMyTeam('blue')
+# 		if value == 'got my team':
+# 			print(client.myTeam())
+# 			print(client.displayTeam())
+# 			client.waitStart()
+# 			t1 = Thread(target=SEND2)
+# 			t2 = Thread(target=RECEIVE)
+# 			t1.start()
+# 			t2.start()
+
+# client.clientTCP.close()
--- Servidor/gameServer.py
+++ Servidor/gameServer.py
@@ -1,4 +1,5 @@
 import socket, time
+from threading import Thread #threads geram "microservers" que computam a pontuação de cada usuário
 from random import randint
 
 #O programa segue uma ordem "cronológica", na qual a função anterior chama a próxima. Nas divisões, são momentos em que a anterior não chamou a seguinte
@@ -6,22 +7,22 @@ from random import randint
 class Server():
 	def __init__(self):
 		self.host = '127.0.0.1' #[ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][1] #pega o IP da máquina
-		self.port = 5000 #a porta da conexão é a porta 80 web
+		self.port = 80 #a porta da conexão é a porta 80 web (sempre aberta, não precisa)
 
 	def serverStart(self):	#a função inicia o servidor
 		print("Servidor Iniciado")
-		print('Server IP: ', self.host)
+		print('Server IP:', self.host, '\n')
 		self.clients_connected = list()
 
-		self.s = socket.socket()
-		self.s.bind((self.host, self.port))
+		self.servertcp = socket.socket() #A conexão estabelecida é TCP
+		self.servertcp.bind((self.host, self.port)) 
 
-		self.s.listen(1)
-		self.s.setblocking(0)
+		self.servertcp.listen(1)
+		self.servertcp.setblocking(0)
 
 		while True: #escuta os clientes
 			try:
-				c, addr = self.s.accept()
+				c, addr = self.servertcp.accept()
 				if not (c, addr) in self.clients_connected:
 					self.clients_connected.append((c, addr))
 					print(addr)
@@ -34,89 +35,255 @@ class Server():
 					data = self.clients_connected[0][0].recv(1024) #o cliente pricipal pode fazer o servidor parar de escutar por jogadores
 					if data == 'close'.encode():
 						break
-					else:
-						pass
-				except:
-					pass
+				except: pass
 
-		print("end")
-		self.sortTeams()
+		print("Open connection ended")
+		self.callforNames()
 
-	#sorteia os timeas aleatóriamente
-	def sortTeams(self):
+	def callforNames(self):
+		self.NameDict = dict()
+		print('Getting names...')
+		for client in self.clients_connected:
+			client[0].send('send your name'.encode())
+			while True:
+				try:
+					name = client[0].recv(1024)
+					name2 = str(name).split("'")
+					self.NameDict[client] = [str(name2[1])]
+					print(str(name), 'playing')
+					break
+				except: pass
+		self.separateTeams()
+
+	#sorteia os times aleatóriamente
+	def separateTeams(self):	
 		self.team_blue = list()
 		self.team_red = list()
-		players = self.clients_connected[:]
-		number = randint(0, 1)
+		self.random_List = list()
 
-		for player in players:
-			if number == 0:
-				self.team_blue.append(player)
-				number = 1
+		self.blueDict = dict()
+		self.redDict = dict()
+
+		print('Getting Teams...')
+		
+		for client in self.clients_connected:
+			client[0].send('send your team'.encode())
+			while True:
+				try:
+					team = client[0].recv(1024)
+					print('Team', str(team))
+					team2 = str(team).split("'")
+					
+					if team2[1] == 'blue':
+						self.team_blue.append(client)
+						self.NameDict[client].append('blue')
+						self.blueDict[self.NameDict[client][0]] = 0
+						print('Blue', self.blueDict)
+
+					elif team2[1] == 'red':
+						self.team_red.append(client)
+						self.NameDict[client].append('red')
+						self.redDict[self.NameDict[client][0]] = 0
+						print('Red', self.redDict)
+
+					elif team2[1] == 'random':
+						print('Random')
+						self.random_List.append(client)
+					break
+				except: pass 
+
+		for client in self.random_List:		
+			if len(self.team_blue) < len(self.team_red):
+				self.team_blue.append(client)
+				self.NameDict[client].append('blue')
+				self.blueDict[self.NameDict[client][0]] = 0
+
+			elif len(self.team_red) < len(self.team_blue):
+				self.team_red.append(client)
+				self.NameDict[client].append('red')
+				self.redDict[self.NameDict[client][0]] = 0
+
 			else:
-				self.team_red.append(player)
-				number = 0
-			if (len(self.team_blue)+len(self.team_red)) == len(players):
-				break
-			else:
-				continue
+				number = randint(0, 1)
+				if number == 0:
+					self.team_blue.append(client)
+					self.NameDict[client].append('blue')
+					self.blueDict[self.NameDict[client][0]] = 0
+				else:
+					self.team_red.append(client)
+					self.NameDict[client].append('red')
+					self.redDict[self.NameDict[client][0]] = 0		
 
 		self.sendTeams()
 
 	def sendTeams(self):
+		print('Sending teams...')
+		namesBlue = str()
+		namesRed = str()
+
+		for lista in self.NameDict.values():
+			if lista[1] == 'red':
+				namesRed+=":"+lista[0]
+			else:
+				namesBlue+=":"+lista[0]
+
 		for blue in self.team_blue:
-			blue[0].send('team blue'.encode())
+			toSend = 'team blue'+"'"+namesBlue+"'"+namesRed
+			blue[0].send(str(toSend).encode())
 
 		for red in self.team_red:
-			red[0].send('team red'.encode())
+			toSend = 'team red'+"'"+namesRed+"'"+namesBlue
+			red[0].send(str(toSend).encode())
+
 		print("Team Red: ", self.team_red)
 		print("Team Blue: ", self.team_blue)
 		self.waitStart()
 
 	def waitStart(self): 
+		print('Waiting the game Start')
 		while True:
 			try:
-				print('Waiting')
 				data = (self.clients_connected[0][0]).recv(1024)
 				if data:
 					if data == 'start game'.encode():
 						break
-				else:
-					pass
-			except:
-				pass
+			except: pass
 
 		print('Starting game')
 		self.sendStart()
 
 	def sendStart(self):
-		time.sleep(.5) #"re-bounce"
+		time.sleep(1) #"re-bounce"
 		for client in self.clients_connected:
 			client[0].send('start'.encode())
 		print('Game started')
-		self.receivePackages()
+		self.killThreads = False
+		self.generalDict = dict()
+		self.doneThreads = 0
+		self.receiving()
 
-	def receivePackages(self):
-		listaqqr = list()
+	def receiving(self):
+		print('Receiving')
+		for client in self.clients_connected:
+			if client in self.team_red:
+				team = 'red'
+			else:
+				team = 'blue'
+			self.generalDict[client[0]] = Thread(target=self.receivePackages, args=(client, team))
+			self.generalDict[client[0]].start()
+		self.gameEnded()
+
+	def receivePackages(self, player, team):
+		initial_time = time.clock()
+		self.pointsRED = 0
+		self.pointsBLUE = 0
+		self.servertcp.setblocking(0)
+		my_points = 0
+
+		self.restart = False
+
 		while True:
-			whilebreak = False
-			for i in range(2, len(self.clients_connected)-1):
-				try:
-					pack = (self.clients_connected[0][i]).recv(1024)
-					listaqqr.append(str(pack))
-					print(str(pack))
-					if pack == 'end server'.encode():
-						whilebreak = True
-						break
-					else:
-						pass
-				except:
-					pass
+			if 0 <= (time.clock()-initial_time)%.5 <= .005:
+				player[0].send(self.totalPoints().encode())
+				print('Sending', self.totalPoints())
 
-			if whilebreak:
+			if self.killThreads:
 				break
-		print(len(listaqqr))
-		self.s.close()
+
+			try:
+				pack = player[0].recv(1024)
+				print(pack)
+				if pack == 'restart game'.encode():
+					self.restart = True
+					self.killThreads = True
+					break
+				elif pack == 'close'.encode():
+					self.restart = False
+					self.killThreads = True
+				else:
+					pack2 = str(pack).split("'")
+					pack3 = pack2[1].split('"')
+					name = pack2[0].split("'")
+					my_points += int(pack3[0])
+					if team == 'blue':
+						self.addPack('blue', str(pack3[0]))
+					elif team == 'red':
+						self.addPack('red', str(pack3[0]))
+			except: pass
+
+		if team == 'blue':
+			self.addBlueDict(player, my_points)
+		elif team == 'red':
+			self.addRedDict(player, my_points)
+
+		self.doneThreads += 1
+
+	def gameEnded(self):
+		num_players = len(self.clients_connected)
+		while True:
+			if self.doneThreads == num_players:
+				print(self.pointsRED, self.pointsBLUE)	
+				print(self.NameDict.values())
+				print(self.blueDict, self.redDict)
+				break
+
+		print('Sending Highscore...')
+
+		if self.restart:
+			toSend = 'restart game'
+		else:
+			toSend = 'game ended'
+
+		print('Game status: ', toSend)	
+
+		for player in self.clients_connected:
+			player[0].send(toSend.encode())
+			time.sleep(.1)
+			player[0].send((str(self.blueDict)+"!!"+str(self.redDict)).encode())
+
+		print('Highscore sent')
+
+		if self.restart:
+			print('Restarting Game...')
+			self.restartGame()
+		else:
+			print('Ending game...')
+			print('Closing server...')
+			self.servertcp.close()
+			print('Server Closed')
+
+	def addPack(self, team, pack):
+		if team == 'blue':
+			self.pointsBLUE = self.pointsBLUE + int(pack)
+		elif team == 'red':
+			self.pointsRED = self.pointsRED + int(pack)
+
+	def addBlueDict(self, player, points):
+		player_name = self.NameDict[player][0]
+		self.blueDict[player_name] = points
+
+	def addRedDict(self, player, points):
+		player_name = self.NameDict[player][0]
+		self.redDict[player_name] = points
+
+	def totalPoints(self):
+		if self.pointsBLUE == 0 or self.pointsRED == 0:
+			percentage = .5
+		else:
+			percentage = (self.pointsRED/(self.pointsBLUE+self.pointsRED))
+		return str(percentage)
+
+	def restartGame(self):
+		self.pointsRED = 0
+		self.pointsBLUE = 0
+
+		for player in self.redDict.keys():
+			self.redDict[player] = 0
+
+		for player in self.blueDict.keys():
+			self.blueDict[player] = 0
+
+		self.waitStart()
 
 server = Server()
 server.serverStart()
